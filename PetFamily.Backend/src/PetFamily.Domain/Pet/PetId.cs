543774281d38fb38a78adfba17969b72b6095839
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-12-29 14:54:36 +0300

    Создать VO для Id питомца

---
+++ PetFamily.Backend/src/PetFamily.Domain/Pet/PetId.cs
@@ -0,0 +1,14 @@
+﻿namespace PetFamily.Domain.Pet
+{
+    public record PetId
+    {
+        private PetId(Guid value) {
+            Value = value;
+        }
+
+        Guid Value { get; }
+
+        public static PetId NewPetId => new PetId(Guid.NewGuid());
+        public static PetId Empty => new PetId(Guid.Empty);
+    }
+}
