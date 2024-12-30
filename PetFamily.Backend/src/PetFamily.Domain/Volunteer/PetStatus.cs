ceab3f632cb2182581e57f3d21cdd3a6260cfb58
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-12-30 16:30:06 +0300

    Заменить использование PetStatusEnum на VO

--- PetFamily.Backend/src/PetFamily.Domain/Volunteer/PetStatus.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Volunteer/PetStatus.cs
@@ -1,10 +1,36 @@
-﻿namespace PetFamily.Domain.Volunteer
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Volunteer
 {
-    public enum PetStatus 
-    { 
-        NeedsHelp,
-        SearchingForHome,
-        FoundHome
+    public class PetStatus 
+    {
+        public static readonly PetStatus NeedsHelp = new(nameof(NeedsHelp));
+        public static readonly PetStatus SearchingForHome = new(nameof(SearchingForHome));
+        public static readonly PetStatus FoundHome = new(nameof(FoundHome));
+
+        private static readonly PetStatus[] _all = [NeedsHelp, SearchingForHome, FoundHome];
+
+        public string Value { get; set; }
+
+        private PetStatus(string value)
+        {
+            Value = value;
+        }
+
+        public static Result<PetStatus> Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return Result.Failure<PetStatus>("Value cannot be empty");
+            }
+
+            if(_all.Any(g => g.Value.Equals(value, StringComparison.OrdinalIgnoreCase)) == false)
+            {
+                return Result.Failure<PetStatus>("Value is invalid");
+            }
+
+            return Result.Success(new PetStatus(value));
+        }
     }
 
 }
