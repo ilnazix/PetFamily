d8676e17113bf69f1acdbab4639f58a0c190ffb7
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-12-31 15:01:50 +0300

    Добавить DbContext

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PetFamily.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private const string DATABASE = "Database";
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Database"));
+        }
+    }
+}
