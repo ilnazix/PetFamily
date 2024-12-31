185087b1853e4d83d08647aa755a7c253fc04e68
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-12-31 18:24:46 +0300

    Конфигурация Pet

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/PetConfiguration.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Volunteer;
+
+namespace PetFamily.Infrastructure.Configurations
+{
+    public class PetConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.ToTable("pets");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                .HasConversion(id => id.Value, value => PetId.Create(value));
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+
+            builder.ComplexProperty(p => p.PetType, ptb =>
+            {
+                ptb.Property(pt => pt.SpeciesId).IsRequired().HasColumnName("species_id");
+                ptb.Property(pt => pt.BreedId).IsRequired().HasColumnName("breeed_id");
+            });
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_MEDIUM_TEXT_LENGTH);
+
+            builder.Property(p => p.Color)
+                .IsRequired(false)
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+
+            builder.ComplexProperty(p => p.MedicalInformation, mib =>
+            {
+                mib.Property(mi => mi.IsVaccinated).HasColumnName("is_vaccinated");
+                mib.Property(mi => mi.IsCastrated).HasColumnName("is_castrated");
+                mib.Property(mi => mi.Height).HasColumnName("height");
+                mib.Property(mi => mi.Weight).HasColumnName("weight");
+                mib.Property(mi => mi.HealthInformation)
+                    .HasMaxLength(Constants.MAX_MEDIUM_TEXT_LENGTH);
+            });
+
+            builder.ComplexProperty(p => p.Address, ab =>
+            {
+                ab.Property(a => a.Country).HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH).HasColumnName("country");
+                ab.Property(a => a.State).HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH).HasColumnName("state");
+                ab.Property(a => a.City).HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH).HasColumnName("city");
+                ab.Property(a => a.Street).HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH).HasColumnName("street");
+                ab.Property(a => a.HouseNumber).HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH).HasColumnName("house_number");
+                ab.Property(a => a.HouseNumber).HasColumnName("apartment_number");
+            });
+
+            builder.Property(p => p.OwnerPhoneNumber)
+                .HasConversion(phoneNumber => phoneNumber.Value, value => PhoneNumber.Create(value).Value);
+
+            builder.Property(p => p.DateOfBirth);
+            builder.Property(p => p.CreatedAt);
+
+            builder.OwnsOne(p => p.RequisitesList, rlb =>
+            {
+                rlb.ToJson();
+
+                rlb.OwnsMany(v => v.Requisites, rb =>
+                {
+                    rb.Property(r => r.Title)
+                        .IsRequired()
+                        .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+
+                    rb.Property(r => r.Description)
+                        .IsRequired()
+                        .HasMaxLength(Constants.MAX_MEDIUM_TEXT_LENGTH);
+                });
+            });
+        }
+    }
+}
