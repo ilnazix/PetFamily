0552994851e448ac1bc764a5917f198e69d5e815
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2024-12-31 18:30:56 +0300

    Сконфинурировать Species

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/SpeciesConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Species;
+
+namespace PetFamily.Infrastructure.Configurations
+{
+    public class SpeciesConfiguration : IEntityTypeConfiguration<Species>
+    {
+        public void Configure(EntityTypeBuilder<Species> builder)
+        {
+            builder.ToTable("species");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Id).HasConversion(id => id.Value, value => SpeciesId.Create(value));
+
+            builder.Property(s => s.Title).IsRequired().HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+
+            builder.HasMany(s => s.Breeds)
+                .WithOne()
+                .HasForeignKey("species_id");
+        }
+    }
+}
