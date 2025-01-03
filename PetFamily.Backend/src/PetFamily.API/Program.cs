466803bfb88ad499cf0ada644af017f6820d84a5
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-03 16:11:03 +0300

    Настроить DI

--- PetFamily.Backend/src/PetFamily.API/Program.cs
+++ PetFamily.Backend/src/PetFamily.API/Program.cs
@@ -1,11 +1,29 @@
+using PetFamily.Application;
+using PetFamily.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddRouting(opt =>
+{
+    opt.LowercaseUrls = true;
+});
+
+builder.Services
+    .AddInfrastructure()
+    .AddApplication();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PetFamily.Application.Volunteers;
+using PetFamily.Infrastructure.Repositories;
+
+namespace PetFamily.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();
+            services.AddScoped<IVolunteersRepository, VolunteersRepository>();
+
+            return services;
+        }
+    }
+}
