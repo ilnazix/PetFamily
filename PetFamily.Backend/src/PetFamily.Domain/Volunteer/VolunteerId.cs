b67ec0544f26bb8266bbe3f570cfc6b82b07a35f
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-03 16:08:13 +0300

    Добавить перегрузку оператора неявного приведения к Guid для VolunteerId

--- PetFamily.Backend/src/PetFamily.Domain/Volunteer/VolunteerId.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Volunteer/VolunteerId.cs
@@ -11,13 +11,15 @@ namespace PetFamily.Domain.Volunteer
 
         public Guid Value { get; }
 
-        public static VolunteerId NewVolunteerId => new VolunteerId(Guid.NewGuid());
-        public static VolunteerId Empty => new VolunteerId(Guid.Empty);
+        public static VolunteerId NewVolunteerId() => new VolunteerId(Guid.NewGuid());
+        public static VolunteerId Empty() => new VolunteerId(Guid.Empty);
         public static VolunteerId Create(Guid id) => new VolunteerId(id);
 
         protected override IEnumerable<IComparable> GetComparableEqualityComponents()
         {
             yield return Value;
         }
+
+        public static implicit operator Guid(VolunteerId id) => id.Value; 
     }
 }
