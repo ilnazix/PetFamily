7c067984aa02abfb4efeef5c3838387f7765c24f
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-03 16:09:06 +0300

    Создать контроллер для работы с волонтерами

---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/CreateVolunteerRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace PetFamily.API.Controllers.Volunteers
+{
+    public record CreateVolunteerRequest(
+        string FirstName,
+        string LastName, 
+        string MiddleName, 
+        string PhoneNumber,
+        string Email,
+        IEnumerable<SocialMediaDto> SocialMedias,
+        IEnumerable<RequisitesDto> Requisites
+        ); 
+}
---
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/VolunteersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFamily.Application.Volunteers.CreateVolunteer;
+
+namespace PetFamily.API.Controllers.Volunteers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolunteersController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            [FromServices] CreateVolunteerHandler handler,
+            [FromBody] CreateVolunteerRequest request)
+        {
+            return Ok("Hello");
+        }
+    }
+}
