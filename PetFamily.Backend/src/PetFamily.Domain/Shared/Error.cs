07a6f3a2d5e2ee95657755987f657b0a95e161ce
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-07 17:01:13 +0300

    Add methods to serialize and deserialize error

--- PetFamily.Backend/src/PetFamily.Domain/Shared/Error.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Shared/Error.cs
@@ -2,6 +2,8 @@
 {
     public record Error
     {
+        public const string SEPARATOR = "||";
+
         public string Code {  get;  }
         public string Message { get;  }
         public ErrorType Type { get; }
@@ -16,5 +18,27 @@
         public static Error NotFound(string code, string message) => new Error(code, message, ErrorType.NotFound);
         public static Error Failure(string code, string message) => new Error(code, message, ErrorType.Failure);
         public static Error Conflict(string code, string message) => new Error(code, message, ErrorType.Conflict);
+
+        public string Serialize()
+        {
+            return string.Join(SEPARATOR, Code, Message, Type);
+        }
+
+        public static Error Deserialize(string error)
+        {
+            var parts = error.Split(SEPARATOR);
+
+            if(parts.Length < 3)
+            {
+                throw new InvalidOperationException("Invalid serialized format");
+            }
+
+            if (!Enum.TryParse<ErrorType>(parts[2], out var type))
+            {
+                throw new InvalidOperationException("Invalid serialized format");
+            }
+
+            return new Error(parts[0], parts[1], type);
+        }
     }
 }
