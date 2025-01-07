b6e00034a924bc1096e7de71cfc30f97854ab565
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-07 17:04:18 +0300

    Extension method to convert errors to envelope

--- PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/VolunteersController.cs
+++ PetFamily.Backend/src/PetFamily.API/Controllers/Volunteers/VolunteersController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using PetFamily.API.Extensions;
 using PetFamily.API.Response;
 using PetFamily.Application.Volunteers.CreateVolunteer;
+using PetFamily.Domain.Shared;
 
 namespace PetFamily.API.Controllers.Volunteers
 {
@@ -13,6 +15,7 @@ namespace PetFamily.API.Controllers.Volunteers
         public async Task<ActionResult<Envelope>> Create(
             [FromServices] CreateVolunteerHandler handler,
             [FromBody] CreateVolunteerRequest request,
+            [FromServices] IValidator<CreateVolunteerCommand> validator,
             CancellationToken cancellationToken)
         {
             var command = new CreateVolunteerCommand(
@@ -25,7 +28,7 @@ namespace PetFamily.API.Controllers.Volunteers
                 Requisites: request.Requisites.Select(r => new CreateRequisiteCommand(r.Title, r.Description))
             ); 
 
-            var result = await handler.Handle(command, cancellationToken);
+            var result = await handler.Handle(command, validator, cancellationToken);
 
             if (result.IsFailure)
             {
