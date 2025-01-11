b21fddc3f1675c1c3f41cfb1fa823d710f549ed6
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-11 15:42:36 +0300

    Exception Middleware

---
+++ PetFamily.Backend/src/PetFamily.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using PetFamily.API.Response;
+
+namespace PetFamily.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var responseError = new ResponseError("server.internal", ex.Message, null);
+                var envelope = Envelope.Error([responseError]);
+
+                _logger.LogError("Server internal error: {0}", ex.Message);
+
+                await context.Response.WriteAsJsonAsync(envelope);
+            }
+        }
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
