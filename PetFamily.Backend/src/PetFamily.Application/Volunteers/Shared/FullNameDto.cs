edf26399924a4377ca9d220c3131d4045cdd0b5d
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-15 16:54:35 +0300

    Extract fullname to dto

---
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/Shared/FullNameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PetFamily.Application.Volunteers.Shared
+{
+    public record FullNameDto(
+            string FirstName,
+            string LastName,
+            string MiddleName
+        );
+}
