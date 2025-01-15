2b9396d59842a5ad7d61d7256f949aaddf81056c
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-15 18:20:29 +0300

    Update social medias command handler and validator

---
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/UpdateSocialMedias/UpdateSocialMediasCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PetFamily.Application.Validation;
+using PetFamily.Domain.Volunteer;
+
+namespace PetFamily.Application.Volunteers.UpdateSocialMedias
+{
+    public class UpdateSocialMediasCommandValidator : AbstractValidator<UpdateSocialMediasCommand>
+    {
+        public UpdateSocialMediasCommandValidator()
+        {
+            RuleFor(c => c.VolunteerId).NotEmpty();
+            RuleForEach(c => c.Dto.SocialMedias).MustBeValueObject(smq => SocialMedia.Create(smq.Link, smq.Title));
+        }
+    }
+}
