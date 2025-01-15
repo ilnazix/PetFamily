6c5cb95079c8ef3a2c627db024171a9731786b7e
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-15 16:56:36 +0300

    Create a handler for main info updating command and register it in DI

--- PetFamily.Backend/src/PetFamily.Application/DependencyInjection.cs
+++ PetFamily.Backend/src/PetFamily.Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using PetFamily.Application.Volunteers.CreateVolunteer;
+using PetFamily.Application.Volunteers.UpdateMainInfo;
 
 namespace PetFamily.Application
 {
@@ -9,6 +10,7 @@ namespace PetFamily.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<CreateVolunteerHandler>();
+            services.AddScoped<UpdateMainInfoHandler>();
 
             services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
