750319a43697cbb6bb6986462bcc4f6b9d80e746
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-15 18:20:59 +0300

    Register update social media command handler in DI

--- PetFamily.Backend/src/PetFamily.Application/DependencyInjection.cs
+++ PetFamily.Backend/src/PetFamily.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using PetFamily.Application.Volunteers.CreateVolunteer;
 using PetFamily.Application.Volunteers.UpdateMainInfo;
+using PetFamily.Application.Volunteers.UpdateSocialMedias;
 
 namespace PetFamily.Application
 {
@@ -9,8 +10,10 @@ namespace PetFamily.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            //TODO: отрефакторить названия хендлеров с использованием command для единообразия
             services.AddScoped<CreateVolunteerHandler>();
             services.AddScoped<UpdateMainInfoHandler>();
+            services.AddScoped<UpdateSocialMediasCommandHandler>();
 
             services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
