8f3cd66bc1f1ba16a85032f471397cd6bcac5b67
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-15 16:51:03 +0300

    Add methods to get volunteer by id from repo and save them

--- PetFamily.Backend/src/PetFamily.Application/Volunteers/IVolunteersRepository.cs
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/IVolunteersRepository.cs
@@ -1,9 +1,13 @@
-﻿using PetFamily.Domain.Volunteer;
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Volunteer;
 
 namespace PetFamily.Application.Volunteers
 {
     public interface IVolunteersRepository
     {
         Task<Guid> Add(Volunteer volunteer, CancellationToken cancellationToken = default);
+        Task<Result<Volunteer, Error>> GetById(VolunteerId id, CancellationToken cancellationToken = default);
+        Task<Result<Guid, Error>> Save(Volunteer volunteer, CancellationToken cancellationToken = default);
     }
 }
