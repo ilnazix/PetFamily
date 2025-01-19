321c1d3aac1eac31dc03fa6e559a6af7d1fcdef2
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-19 16:53:01 +0300

    Delete expired volunteers service

---
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/DeleteExpiredVolunteersService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace PetFamily.Application.Volunteers
+{
+    public class DeleteExpiredVolunteersService
+    {
+        private readonly IVolunteersRepository _volunteersRepository;
+        private readonly VolunteerEntityOptions _options;
+
+        public DeleteExpiredVolunteersService(IVolunteersRepository volunteersRepository, 
+            IOptions<VolunteerEntityOptions> options)
+        {
+            _volunteersRepository = volunteersRepository;
+            _options = options.Value;
+        }
+
+        public async Task Process()
+        {
+            var olderThan = TimeSpan.FromDays(_options.DeleteExpiredVolunteersServiceReductionDays);
+            await _volunteersRepository.DeleteExpiredSoftDeletions(olderThan);
+        }
+    }
+}
