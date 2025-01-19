cbcf7180c61e1ef49467845278b62637bec164fb
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-19 16:52:24 +0300

    Add bg serivice

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/BackgroundServices/DeleteExpiredVolunteersBackgroundService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PetFamily.Application.Volunteers;
+
+namespace PetFamily.Infrastructure.BackgroundServices
+{
+    public class DeleteExpiredVolunteersBackgroundService : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly ILogger<DeleteExpiredVolunteersBackgroundService> _logger;
+        private readonly VolunteerEntityOptions _options;
+
+        public DeleteExpiredVolunteersBackgroundService(
+            IServiceScopeFactory scopeFactory,
+            ILogger<DeleteExpiredVolunteersBackgroundService> logger,
+            IOptions<VolunteerEntityOptions> options)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+            _options = options.Value;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("DeleteExpiredVolunteersService is started");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await using var scope = _scopeFactory.CreateAsyncScope();
+                
+                var service = scope.ServiceProvider.GetRequiredService<DeleteExpiredVolunteersService>();
+                
+                _logger.LogInformation("DeleteExpiredVolunteersService is working");
+
+                await service.Process();
+
+                await Task.Delay(TimeSpan.FromHours(_options.DeleteExpiredVolunteersServiceReductionDays), 
+                    stoppingToken);
+            }
+        }
+    }
+}
