2101eb1da613e485600ff1f4842389822d6dfb9b
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-30 16:22:57 +0300

    Extension methods to map validation errors to error list

---
+++ PetFamily.Backend/src/PetFamily.Application/Extensions/ValidationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Application.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static ErrorList ToErrorList(this ValidationResult validationResult)
+        {
+            if (validationResult.IsValid) throw new InvalidOperationException("Cannot convert valid validation result to Error List");
+
+            var validationErrors = validationResult.Errors;
+            var errors = validationErrors.Select(ve => Error.Deserialize(ve.ErrorMessage));
+            return new ErrorList(errors);
+        }
+    }
+}
