d639e2ba48e597f84ea031d2f1ce96173b52c380
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-30 20:38:28 +0300

    Change configuration to use VOList

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Migrations/20250130173432_ValueObjectList.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ValueObjectList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "social_media_list",
+                table: "volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "requisites_list",
+                table: "pets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "requisites",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}",
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "social_medias",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}");
+
+            migrationBuilder.AddColumn<string>(
+                name: "photos",
+                table: "pets",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}");
+
+            migrationBuilder.AddColumn<string>(
+                name: "requisites",
+                table: "pets",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "{}");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "social_medias",
+                table: "volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "photos",
+                table: "pets");
+
+            migrationBuilder.DropColumn(
+                name: "requisites",
+                table: "pets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "requisites",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+
+            migrationBuilder.AddColumn<string>(
+                name: "social_media_list",
+                table: "volunteers",
+                type: "jsonb",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "requisites_list",
+                table: "pets",
+                type: "jsonb",
+                nullable: true);
+        }
+    }
+}
