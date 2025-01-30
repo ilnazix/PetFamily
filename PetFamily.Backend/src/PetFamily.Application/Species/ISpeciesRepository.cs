94db949a457eab922a2f2eda987f9b9e12ca8e4e
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-01-30 16:30:14 +0300

    Create Species repo

---
+++ PetFamily.Backend/src/PetFamily.Application/Species/ISpeciesRepository.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Species;
+
+namespace PetFamily.Application.Species
+{
+    public interface ISpeciesRepository
+    {
+        Task<Result<Domain.Species.Species, Error>> GetById(SpeciesId id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Repositories/SpeciesRepository.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Application.Species;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Species;
+
+
+namespace PetFamily.Infrastructure.Repositories
+{
+    public class SpeciesRepository : ISpeciesRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SpeciesRepository(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        public async Task<Result<Species, Error>> GetById(SpeciesId id, CancellationToken cancellationToken = default)
+        {
+            var species = await _context.Species
+                .Include(s => s.Breeds)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if(species is null)
+            {
+                return Errors.General.NotFound(id.Value);
+            }
+
+            return species;
+        }
+    }
+}
