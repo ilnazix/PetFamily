781cd773534a96438b0cfc4577ad5212ba36fc3a
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-02-16 15:38:47 +0300

    Queries

---
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/Queries/GetVolunteersWithPagination/GetFilteredVolunteersWithPaginationQuery.cs
@@ -0,0 +1,13 @@
+﻿using PetFamily.Application.Abstractions;
+
+namespace PetFamily.Application.Volunteers.Queries.GetVolunteersWithPagination
+{
+    public record GetFilteredVolunteersWithPaginationQuery(
+        string? FirstName,
+        string? LastName,
+        string? MiddleName,
+        string? Email,
+        int Page,
+        int PageSize) : IQuery;
+
+}
