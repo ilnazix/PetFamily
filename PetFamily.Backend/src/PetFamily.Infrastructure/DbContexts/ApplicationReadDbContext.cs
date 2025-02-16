e7e7b031fd9b8d68a92223296dd5c6b07448a8bd
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-02-16 15:36:09 +0300

    Split read and write db context

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/DbContexts/ApplicationReadDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PetFamily.Application.Database;
+using PetFamily.Application.Dtos;
+
+namespace PetFamily.Infrastructure.DbContexts
+{
+    public class ApplicationReadDbContext : DbContext, IReadDbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public IQueryable<VolunteerDto> Volunteers => Set<VolunteerDto>();
+
+        public ApplicationReadDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql(_configuration.GetConnectionString(Constants.DATABASE))
+                .UseLoggerFactory(CreateLoggerFactory())
+                .UseSnakeCaseNamingConvention()
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationReadDbContext).Assembly, 
+                type => type.FullName?.Contains("Configurations.Read") ?? false);
+        }
+
+        private ILoggerFactory CreateLoggerFactory()
+        {
+            return LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+        }
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/DbContexts/ApplicationWriteDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PetFamily.Domain.Species;
+using PetFamily.Domain.Volunteers;
+using EFCore.NamingConventions;
+
+namespace PetFamily.Infrastructure.DbContexts
+{
+    public class ApplicationWriteDbContext : DbContext
+    {
+        private const string DATABASE = "Database";
+        private readonly IConfiguration _configuration;
+
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Species> Species { get; set; }
+
+        public ApplicationWriteDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseNpgsql(_configuration.GetConnectionString(DATABASE))
+                .UseLoggerFactory(CreateLoggerFactory())
+                .UseSnakeCaseNamingConvention();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationWriteDbContext).Assembly,
+                type => type.FullName?.Contains("Configurations.Write") ?? false);
+        }
+
+        private ILoggerFactory CreateLoggerFactory()
+        {
+            return LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+        }
+    }
+}
