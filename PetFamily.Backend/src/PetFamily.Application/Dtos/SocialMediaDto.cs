09e57f54b2f1f0635a87c6a1bc76ce74e2680c08
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-02-16 15:41:30 +0300

    VolunteerDto and related dtos

---
+++ PetFamily.Backend/src/PetFamily.Application/Dtos/SocialMediaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PetFamily.Application.Dtos
+{
+    public class SocialMediaDto 
+    {
+        public string Link { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+    }
+}
