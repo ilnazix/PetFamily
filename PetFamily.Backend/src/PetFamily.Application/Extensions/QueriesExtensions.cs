f693bdd014f4d4117f79c4fcb213ab2800e71326
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-02-16 15:44:09 +0300

    PagedList

---
+++ PetFamily.Backend/src/PetFamily.Application/Extensions/QueriesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetFamily.Application.Models;
+using System.Linq.Expressions;
+
+namespace PetFamily.Application.Extensions
+{
+    public static class QueriesExtensions
+    {
+        public static async Task<PagedList<T>> ToPagedList<T>(
+            this IQueryable<T> source,
+            int page,
+            int pageSize,
+            CancellationToken cancellationToken = default)
+        {
+            var totalCount = await source.CountAsync(cancellationToken);
+
+            var items = await source
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            var pagedList = new PagedList<T>()
+            {
+                Items = items,
+                TotalCount = totalCount,
+                PageSize = pageSize,
+                Page = page
+            };
+
+            return pagedList;
+        }
+
+        public static IQueryable<T> WhereIf<T>(
+            this IQueryable<T> source,
+            bool condition,
+            Expression<Func<T, bool>> predicate)
+        {
+            return condition ? source.Where(predicate) : source;
+        }
+    }
+}
