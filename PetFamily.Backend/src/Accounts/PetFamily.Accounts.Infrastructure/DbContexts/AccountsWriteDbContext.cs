cfdbfedc241aa315296edc1735c607b137c98884
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-09-25 12:27:57 +0300

    ReadDbcontext

---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Infrastructure/DbContexts/AccountsWriteDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Accounts.Domain;
+using PetFamily.Accounts.Infrastructure.Configurations.Write;
+
+namespace PetFamily.Accounts.Infrastructure.DbContexts;
+
+internal class AccountsWriteDbContext : IdentityDbContext<User, Role, Guid>
+{
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<RefreshSession> RefreshSessions { get; set; }
+
+
+    public AccountsWriteDbContext(DbContextOptions<AccountsWriteDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.HasDefaultSchema(Constants.SCHEMA);
+
+        builder.ApplyConfiguration(new UserConfiguration());
+
+        builder
+            .Entity<Role>()
+            .ToTable("roles")
+            .HasMany(r => r.Permissions)
+            .WithMany(p => p.Roles)
+            .UsingEntity<RolePermission>();
+
+        builder.Entity<RolePermission>().ToTable("role_permission");
+
+        builder
+            .Entity<Permission>()
+            .ToTable("permissions")
+            .HasIndex(p => p.Code)
+            .IsUnique();
+
+        builder.Entity<ParticipantAccount>().ToTable("participant_accounts");
+        builder.ApplyConfiguration(new VolunteerAccountConfiguration());
+        builder.ApplyConfiguration(new AdminAccountConfiguration());
+
+        builder
+            .Entity<IdentityUserClaim<Guid>>()
+            .ToTable("user_claims");
+
+        builder
+            .Entity<IdentityUserToken<Guid>>()
+            .ToTable("user_tokens");
+
+        builder
+            .Entity<IdentityUserLogin<Guid>>()
+            .ToTable("user_logins");
+
+        builder
+            .Entity<IdentityRoleClaim<Guid>>()
+            .ToTable("role_claims");
+
+        builder
+            .Entity<IdentityUserRole<Guid>>()
+            .ToTable("user_roles");
+    }
+}
