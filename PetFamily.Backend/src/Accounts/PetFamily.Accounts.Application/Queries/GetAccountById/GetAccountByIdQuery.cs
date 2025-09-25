56a3ecda98225dff37c044bb051eba3321be6371
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-09-25 12:27:29 +0300

    Get account by id

---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Application/Queries/GetAccountById/GetAccountByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using PetFamily.Core.Abstractions;
+
+namespace PetFamily.Accounts.Application.Queries.GetAccountById;
+
+public record GetAccountByIdQuery(Guid Id) : IQuery;
---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Application/Queries/GetAccountById/GetAccountByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetFamily.Accounts.Application.Database;
+using PetFamily.Accounts.Application.DTOs;
+using PetFamily.Core.Abstractions;
+
+namespace PetFamily.Accounts.Application.Queries.GetAccountById;
+
+public class GetAccountByIdQueryHandler : IQueryHandler<AccountDto?, GetAccountByIdQuery>
+{
+    private readonly IAccountsReadDbContext _readDbContext;
+
+    public GetAccountByIdQueryHandler(IAccountsReadDbContext readDbContext)
+    {
+        _readDbContext = readDbContext;
+    }
+
+    public Task<AccountDto?> Handle(
+        GetAccountByIdQuery query, 
+        CancellationToken cancelationToken = default)
+    {
+        return _readDbContext.Accounts
+            .FirstOrDefaultAsync(
+                a => a.Id == query.Id, 
+                cancelationToken);
+    }
+}
