033b6c8ffe83eaff2f579d7990bec1f256ab3c8f
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-09-25 12:26:01 +0300

    Read model configurations

---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Infrastructure/Configurations/Read/AccountDtoConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Accounts.Application.DTOs;
+
+
+namespace PetFamily.Accounts.Infrastructure.Configurations.Read;
+
+internal class AccountDtoConfiguration : IEntityTypeConfiguration<AccountDto>
+{
+    public void Configure(EntityTypeBuilder<AccountDto> builder)
+    {
+        builder.ToTable(Tables.USERS);
+
+        builder.HasKey(a => a.Id);
+
+        builder
+           .HasOne(u => u.AdminAccount)
+           .WithOne()
+           .HasForeignKey<AdminAccountDto>("user_id");
+
+        builder
+            .HasOne(u => u.ParticipantAccount)
+            .WithOne()
+            .HasForeignKey<ParticipantAccountDto>("user_id");
+
+        builder
+            .HasOne(u => u.VolunteerAccount)
+            .WithOne()
+            .HasForeignKey<VolunteerAccountDto>("user_id");
+
+        builder.Navigation(u => u.AdminAccount).AutoInclude();
+        builder.Navigation(u => u.ParticipantAccount).AutoInclude();
+        builder.Navigation(u => u.VolunteerAccount).AutoInclude();
+    }
+}
---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Infrastructure/Configurations/Read/ParticipantAccountDtoConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Accounts.Application.DTOs;
+
+
+namespace PetFamily.Accounts.Infrastructure.Configurations.Read;
+
+internal class ParticipantAccountDtoConfiguration : IEntityTypeConfiguration<ParticipantAccountDto>
+{
+    public void Configure(EntityTypeBuilder<ParticipantAccountDto> builder)
+    {
+        builder.ToTable(Tables.PARTICIPANT_ACCOUNTS);
+        builder.HasKey(a => a.Id);
+    }
+}
