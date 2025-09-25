58c2dc1e61e1d492a8e08dae2e3e8617cca63a23
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-09-25 12:25:27 +0300

    Read models

---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Application/DTOs/AdminAccountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PetFamily.Accounts.Application.DTOs;
+
+public class AdminAccountDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string MiddleName { get; set; } = string.Empty;
+}
+
---
+++ PetFamily.Backend/src/Accounts/PetFamily.Accounts.Application/DTOs/VolunteerAccountDto.cs
@@ -0,0 +1,10 @@
+﻿using PetFamily.SharedKernel.ValueObjects;
+
+namespace PetFamily.Accounts.Application.DTOs;
+
+public class VolunteerAccountDto
+{
+    public Guid Id { get; set; }
+    public List<Requisite> Requisites { get; set; } = new List<Requisite>();
+    public List<SocialMedia> SocialMedias { get; set; } = new List<SocialMedia>();
+}
