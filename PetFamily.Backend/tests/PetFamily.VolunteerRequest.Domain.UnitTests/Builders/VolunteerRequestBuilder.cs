7f4e085745301b4f17d7fae2e67887d5e10940dd
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-10-01 22:38:05 +0300

    Tests

---
+++ PetFamily.Backend/tests/PetFamily.VolunteerRequest.Domain.UnitTests/Builders/VolunteerRequestBuilder.cs
@@ -0,0 +1,88 @@
+﻿using PetFamily.SharedKernel.ValueObjects.Ids;
+
+namespace PetFamily.VolunteerRequest.Domain.UnitTests.Builders;
+
+public class VolunteerRequestBuilder
+{
+    private Guid _userId = Guid.NewGuid();
+    private VolunteerInfo _volunteerInfo = new();
+
+    public VolunteerRequestBuilder WithUserId(Guid userId)
+    {
+        _userId = userId;
+        return this;
+    }
+
+    public VolunteerRequestBuilder WithVolunteerInfo(VolunteerInfo info)
+    {
+        _volunteerInfo = info;
+        return this;
+    }
+
+    public VolunteerRequest CreateDefault()
+    {
+        var id = VolunteerRequestId.NewVolunteerId();
+        return VolunteerRequest.Create(id, _userId, _volunteerInfo).Value;
+    }
+
+    public VolunteerRequest CreateWithSubmittedStatus()
+    {
+        var request = CreateDefault();
+        request.Submit();
+        return request;
+    }
+
+    public VolunteerRequest CreateWithOnReviewStatus(Guid adminId)
+    {
+        var request = CreateWithSubmittedStatus();
+        request.TakeOnReview(adminId);
+        return request;
+    }
+
+    public VolunteerRequest CreateWithApprovedStatus(Guid adminId)
+    {
+        var request = CreateWithOnReviewStatus(adminId);
+        request.Approve();
+        return request;
+    }
+
+    public VolunteerRequest CreateWithRejectedStatus(Guid adminId, string rejectionComment)
+    {
+        var request = CreateWithOnReviewStatus(adminId);
+        request.Reject(rejectionComment);
+        return request;
+    }
+
+    public VolunteerRequest CreateWithRevisionRequiredStatus(Guid adminId, string rejectionComment)
+    {
+        var request = CreateWithOnReviewStatus(adminId);
+        request.RequestRevision(rejectionComment);
+        return request;
+    }
+
+    public VolunteerRequest CreateWithStatus(
+    VolunteerRequestStatus status,
+    Guid? adminId = null,
+    string? comment = null)
+    {
+        if (status == VolunteerRequestStatus.Created)
+            return CreateDefault();
+
+        if (status == VolunteerRequestStatus.Submitted)
+            return CreateWithSubmittedStatus();
+
+        if (status == VolunteerRequestStatus.OnReview)
+            return CreateWithOnReviewStatus(adminId ?? Guid.NewGuid());
+
+        if (status == VolunteerRequestStatus.Approved)
+            return CreateWithApprovedStatus(adminId ?? Guid.NewGuid());
+
+        if (status == VolunteerRequestStatus.Rejected)
+            return CreateWithRejectedStatus(adminId ?? Guid.NewGuid(), comment ?? "Rejected for test purposes");
+
+        if (status == VolunteerRequestStatus.RevisionRequired)
+            return CreateWithRevisionRequiredStatus(adminId ?? Guid.NewGuid(), comment ?? "Revision required for test purposes");
+
+        throw new ArgumentOutOfRangeException(nameof(status), status, "Unsupported status");
+    }
+}
