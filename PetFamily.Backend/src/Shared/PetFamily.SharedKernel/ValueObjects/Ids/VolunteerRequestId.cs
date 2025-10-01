36d8216df61d8aa54080ea90333be92395f5a159
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-10-01 22:28:08 +0300

    Domain entities

---
+++ PetFamily.Backend/src/Shared/PetFamily.SharedKernel/ValueObjects/Ids/VolunteerRequestId.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.SharedKernel.ValueObjects.Ids
+{
+    public class VolunteerRequestId : ComparableValueObject
+    {
+        private VolunteerRequestId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        public static VolunteerRequestId NewVolunteerId() => new VolunteerRequestId(Guid.NewGuid());
+        public static VolunteerRequestId Empty() => new VolunteerRequestId(Guid.Empty);
+        public static VolunteerRequestId Create(Guid id) => new VolunteerRequestId(id);
+
+        protected override IEnumerable<IComparable> GetComparableEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public static implicit operator Guid(VolunteerRequestId id) => id.Value;
+    }
+}
---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Domain/VolunteerRequestStatus.cs
@@ -0,0 +1,55 @@
+﻿using CSharpFunctionalExtensions;
+using PetFamily.SharedKernel;
+
+namespace PetFamily.VolunteerRequest.Domain;
+
+public class VolunteerRequestStatus : ComparableValueObject
+{
+    public static readonly VolunteerRequestStatus Created = new(nameof(Created).ToLower());
+    public static readonly VolunteerRequestStatus Submitted = new(nameof(Submitted).ToLower());
+    public static readonly VolunteerRequestStatus Rejected = new(nameof(Rejected).ToLower());
+    public static readonly VolunteerRequestStatus OnReview = new(nameof(OnReview).ToLower());
+    public static readonly VolunteerRequestStatus Approved = new(nameof(Approved).ToLower());
+    public static readonly VolunteerRequestStatus RevisionRequired = new(nameof(RevisionRequired).ToLower());
+
+    private static readonly VolunteerRequestStatus[] _all =
+    [
+        Created,
+        Submitted,
+        Rejected,
+        OnReview,
+        Approved,
+        RevisionRequired
+    ];
+
+    public string Value { get; }
+
+    private VolunteerRequestStatus(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<VolunteerRequestStatus, Error> Create(string status)
+    {
+        if (string.IsNullOrWhiteSpace(status))
+        {
+            return Errors.General.ValueIsRequired(nameof(status));
+        }
+
+        status = status.Trim().ToLower();
+
+        if (_all.Any(s => s.Value == status) == false)
+        {
+            return Errors.General.ValueIsInvalid(nameof(status));
+        }
+
+        return new VolunteerRequestStatus(status);
+    }
+
+    protected override IEnumerable<IComparable> GetComparableEqualityComponents()
+    {
+        yield return Value;
+    }
+}
+
+
