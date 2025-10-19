531059c6a8b056e3a63db1c155de9224872fb879
Ilnaz Khuzin <ilnaz.khuzin2016@gmail.com>
2025-10-19 15:22:19 +0300

    Fix migrations

---
+++ PetFamily.Backend/src/Discussions/PetFamily.Discussions.Infrastructure/Migrations/20251019113119_VolunteerRequest_20251019143111.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.Discussions.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerRequest_20251019143111 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PetFamily.Backend/src/VolunteerRequest/PetFamily.VolunteerRequest.Infrastructure/Migrations/20251019115725_VolunteerRequest_20251019145719.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetFamily.VolunteerRequest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class VolunteerRequest_20251019145719 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
